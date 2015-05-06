83b1c9758ff72147669a0682a2bb5d34065e5148
Alan Yang <ayang1097@gmail.com>
2015-05-06 08:23:56 -0500

    Commit Message

---
+++ LinearSolver.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+/**
+ * Solves Ax=b using Gaussian Elimination and Back Substitution
+ * 
+ * @author Alan Yang
+ *
+ */
+public class LinearSolver {
+	private static final double error = 1E-30;
+	Map<Integer, Integer> swaps;
+	Matrix coefficients;
+	Matrix rightSide;
+	Matrix solution;
+
+	public LinearSolver(Matrix coefficients, Matrix rightSide) {
+		swaps = new HashMap<Integer, Integer>();
+		this.coefficients = coefficients;
+		this.rightSide = rightSide;
+
+		Matrix Augmented = upperTriangular(coefficients, rightSide);
+		solution = backSubstitute(Augmented);
+	}
+
+	public Matrix getSolution() {		
+		return solution;
+	}
+
+	/*
+	 * returns upper triangular matrix, augmented with the right side
+	 */
+	public Matrix upperTriangular(Matrix coefficients, Matrix rightSide) {
+		int rows = coefficients.countRows();
+		int columns = coefficients.countColumns();
+		Matrix augmented = coefficients.augmentColumn(rightSide);
+		
+		// get 0's below each pivot
+		for (int j = 0; j < columns; j++) {
+			// if 0 at pivot position, swap or move on to next column
+			if (Math.abs(augmented.getValue(j, j)) <= error){
+				for (int i = j + 1; i < rows; i++) {
+					// if a suitable pivot is found, do a row swap.
+					if (Math.abs(augmented.getValue(i, j)) >= error){
+						augmented = augmented.rowSwap(i, j);
+						swaps.put(i, j);
+					}
+				}
+			} else{	// if no row swaps needed, proceed as normal for this column
+				for (int i = j + 1; i < rows; i++){
+					double multiplier = augmented.getValue(i, j) / augmented.getValue(j, j);
+					Matrix elementary = Matrix.Identity(rows);
+					elementary.setValue(-multiplier, i, j);
+					augmented = elementary.multiply(augmented);
+				}
+			}
+		}
+		return augmented;
+	}
+
+	/*
+	 * back substitutes for solution, once row echelon form reached
+	 */
+	public Matrix backSubstitute(Matrix Ab) {
+		int components = Ab.countRows();
+		double[][] solution = new double[components][1];
+
+		for (int i = components - 1; i >= 0; i--) {
+			double rightSide = Ab.getValue(i, Ab.countColumns() - 1);
+			for (int j = 0; j < Ab.countColumns() - 1; j++) {
+				if (j != i) {
+					rightSide -= solution[j][0] * Ab.getValue(i, j);
+				}
+			}
+
+			if (Math.abs(Ab.getValue(i, i)) >= error) {
+				solution[i][0] = rightSide / Ab.getValue(i, i);
+			} else {
+				// no solution or infinitely many
+				return null;
+			}
+		}
+		Matrix response = new Matrix(components, 1);
+		response.setMatrix(solution);
+
+		// undo row swaps from upperTriangular
+		for (int row : swaps.keySet()) {
+			response.rowSwap(row, swaps.get(row));
+		}
+
+		swaps.clear();
+		return response;
+	}
+}
